90b7033a0547819234aa53b29fe2bf83e1454a11
Carlos Quiala <cquiala12@gmail.com>
2025-01-21 13:22:49 -0500

    wokr on local with ngrok

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ import fastapi
 import uvicorn
 import telebot
 import toml
+import logging
+telebot.logger.setLevel(logging.DEBUG)
+
 
 CONF = toml.load('config.toml')
 TOKEN = CONF['telegram']['token']
+#TOKEN = "8082269540:AAGOzwSoL2CLoi_kradgxPl6gN9nmK5zjRU"
 URL = CONF['url']['render']
+#URL = "46aa-139-177-200-40.ngrok-free.app"
 API_TOKEN = TOKEN
 
 WEBHOOK_HOST = URL
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
+print(URL)
 
 WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
 WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
@@ -30,8 +36,11 @@ WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 # When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
 # with the same value in you put in WEBHOOK_HOST
 
-WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+#WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_BASE = "https://{}".format(WEBHOOK_HOST)
 WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+print(WEBHOOK_URL_BASE)
+print(WEBHOOK_URL_PATH)
 
 logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
@@ -45,7 +54,7 @@ def root():
     return 'Hello, World!'
 
 @app.post(f'/{API_TOKEN}/')
-def process_webhook(update: dict):
+async def process_webhook(update: dict):
     """
     Process webhook calls
     """
@@ -79,15 +88,15 @@ bot.remove_webhook()
 
 # Set webhook
 bot.set_webhook(
-    url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-    certificate=open(WEBHOOK_SSL_CERT, 'r')
+    url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH
+    #certificate=open(WEBHOOK_SSL_CERT, 'r')
 )
 
 
 uvicorn.run(
     app,
     host=WEBHOOK_LISTEN,
-    port=WEBHOOK_PORT,
-    ssl_certfile=WEBHOOK_SSL_CERT,
-    ssl_keyfile=WEBHOOK_SSL_PRIV
+    port=WEBHOOK_PORT
+    #ssl_certfile=WEBHOOK_SSL_CERT,
+    #ssl_keyfile=WEBHOOK_SSL_PRIV
 )
