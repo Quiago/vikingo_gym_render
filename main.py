eb362e79b48f843fc15d219f48f3218aebf8400f
Carlos Quiala <cquiala12@gmail.com>
2025-01-20 23:13:26 -0500

    GITHUB EXAMPLE

--- main.py
+++ main.py
@@ -1,31 +1,90 @@
-from fastapi import FastAPI, Request
-import telebot
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This is a simple echo bot using decorators and webhook with fastapi
+# It echoes any incoming text messages and does not use the polling method.
+
+import logging
+import fastapi
 import uvicorn
+import telebot
 import toml
 
-CONF = toml.load("config.toml")
-TOKEN = CONF["telegram"]["token"]
-WEBHOOK_URL = CONF["url"]["render"]
+CONF = toml.load('config.toml')
+TOKEN = CONF['telegram']['token']
+URL = CONF['url']['render']
+API_TOKEN = TOKEN
 
-bot = telebot.TeleBot(TOKEN, parse_mode=None)
-app = FastAPI()
+WEBHOOK_HOST = URL
+WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-@app.post("/webhook")
-async def webhook(request: Request):
-    json_str = await request.body()
-    update = telebot.types.Update.de_json(json_str.decode("utf-8"))
-    bot.process_new_updates([update])
-    return {"status": "ok"}
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
-@bot.message_handler(commands=['start'])
+# Quick'n'dirty SSL certificate generation:
+#
+# openssl genrsa -out webhook_pkey.pem 2048
+# openssl req -new -x509 -days 3650 -key webhook_pkey.pem -out webhook_cert.pem
+#
+# When asked for "Common Name (e.g. server FQDN or YOUR name)" you should reply
+# with the same value in you put in WEBHOOK_HOST
+
+WEBHOOK_URL_BASE = "https://{}:{}".format(WEBHOOK_HOST, WEBHOOK_PORT)
+WEBHOOK_URL_PATH = "/{}/".format(API_TOKEN)
+
+logger = telebot.logger
+telebot.logger.setLevel(logging.INFO)
+
+bot = telebot.TeleBot(API_TOKEN)
+
+app = fastapi.FastAPI(docs=None, redoc_url=None)
+
+
+@app.post(f'/{API_TOKEN}/')
+def process_webhook(update: dict):
+    """
+    Process webhook calls
+    """
+    if update:
+        update = telebot.types.Update.de_json(update)
+        bot.process_new_updates([update])
+    else:
+        return
+
+
+@bot.message_handler(commands=['help', 'start'])
 def send_welcome(message):
-    bot.reply_to(message, "¡Hola! Soy tu bot de Telegram.")
+    """
+    Handle '/start' and '/help'
+    """
+    bot.reply_to(message,
+                 ("Hi there, I am EchoBot.\n"
+                  "I am here to echo your kind words back to you."))
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
+
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def echo_message(message):
+    """
+    Handle all other messages
+    """
     bot.reply_to(message, message.text)
 
-@app.on_event("startup")
-async def on_startup():
-    bot.remove_webhook()
-    bot.set_webhook(url=WEBHOOK_URL)
+
+# Remove webhook, it fails sometimes the set if there is a previous webhook
+bot.remove_webhook()
+
+# Set webhook
+bot.set_webhook(
+    url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+    certificate=open(WEBHOOK_SSL_CERT, 'r')
+)
+
+
+uvicorn.run(
+    app,
+    host=WEBHOOK_LISTEN,
+    port=WEBHOOK_PORT,
+    ssl_certfile=WEBHOOK_SSL_CERT,
+    ssl_keyfile=WEBHOOK_SSL_PRIV
+)
